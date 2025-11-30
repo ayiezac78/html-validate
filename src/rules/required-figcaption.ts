5514b7bdd6edc7bcb09682d39de68719a1c3af55
Ariel Maniago <admaniago01@gmail.com>
2025-11-30 17:10:19 +0800

    test: finalize initial tests

--- src/rules/required-figcaption.ts
+++ src/rules/required-figcaption.ts
@@ -10,48 +10,57 @@ export default class RequiredFigcaptionRule extends Rule {
 	}
 
 	public setup() {
-		this.on("element:ready", ({ target }) => {
-			if (target.tagName === "figure") {
-				const hasImg = target.querySelector("img");
-				const hasFigcaption = target.querySelector("figcaption");
-				const hasCite = target.querySelector("cite");
+		this.on("dom:ready", (event) => {
+			const { document } = event;
 
-				// Check for missing img
-				if (!hasImg) {
-					this.report({
-						node: target,
-						message: "The <img> element is required for <figure> elements.",
-					});
-				}
+			const figureElements = document.getElementsByTagName("figure");
+			const divElements = document.getElementsByTagName("div");
 
-				// Check for cite instead of figcaption (more specific error)
-				if (hasImg && hasCite && !hasFigcaption) {
-					this.report({
-						node: target,
-						message:
-							"Use <figcaption> instead of <cite> for image captions within <figure> elements.",
-					});
-				}
-				// Check for missing figcaption (generic error, only if cite check didn't trigger)
-				else if (!hasFigcaption) {
-					this.report({
-						node: target,
-						message:
-							"The <figcaption> element is required for <figure> elements.",
-					});
+			for (const figure of figureElements) {
+				if (figure.tagName === "figure") {
+					const hasImg = figure.querySelector("img");
+					const hasFigcaption = figure.querySelector("figcaption");
+					const hasCite = figure.querySelector("cite");
+
+					// Check for missing img
+					if (!hasImg) {
+						this.report({
+							node: figure,
+							message: "The <img> element is required for <figure> elements.",
+						});
+					}
+
+					// Check for cite instead of figcaption (more specific error)
+					if (hasImg && hasCite && !hasFigcaption) {
+						this.report({
+							node: figure,
+							message:
+								"Use <figcaption> instead of <cite> for image captions within <figure> elements.",
+						});
+					}
+					// Check for missing figcaption (generic error, only if cite check didn't trigger)
+					else if (!hasFigcaption) {
+						this.report({
+							node: figure,
+							message:
+								"The <figcaption> element is required for <figure> elements.",
+						});
+					}
 				}
 			}
 
-			if (target.tagName === "div") {
-				const hasImg = target.querySelector("img");
-				const hasCite = target.querySelector("cite");
+			for (const div of divElements) {
+				if (div.tagName === "div") {
+					const hasImg = div.querySelector("img");
+					const hasCite = div.querySelector("cite");
 
-				if (hasImg && hasCite) {
-					this.report({
-						node: target,
-						message:
-							"Consider using <figure> with <figcaption> instead of <div> with <img> and <cite> for better semantic HTML.",
-					});
+					if (hasImg && hasCite) {
+						this.report({
+							node: div,
+							message:
+								"Consider using <figure> with <figcaption> instead of <div> with <img> and <cite> for better semantic HTML.",
+						});
+					}
 				}
 			}
 		});
--- src/rules/required-img-width-height-attr.ts
+++ src/rules/required-img-width-height-attr.ts
@@ -10,52 +10,56 @@ export default class RequiredWidthHeightRule extends Rule {
 	}
 
 	public setup() {
-		this.on("element:ready", ({ target }) => {
-			const width = target.getAttribute("width");
-			const height = target.getAttribute("height");
+		this.on("dom:ready", (event) => {
+			const { document } = event;
 
-			const hasValidWidth =
-				width && typeof width.value === "string" && width.value.trim() !== "";
-			const hasValidHeight =
-				height &&
-				typeof height.value === "string" &&
-				height.value.trim() !== "";
+			const imgEl = document.getElementsByTagName("img");
 
-			const srcValue = target.getAttributeValue("src");
+			for (const img of imgEl) {
+				const width = img.getAttribute("width");
+				const height = img.getAttribute("height");
 
-			const hasModernImageFmt = /\.(webp|avif)(\?.*)?$/i.test(
-				srcValue as string,
-			);
+				const hasValidWidth =
+					width && typeof width.value === "string" && width.value.trim() !== "";
+				const hasValidHeight =
+					height &&
+					typeof height.value === "string" &&
+					height.value.trim() !== "";
 
-			switch (true) {
-				case !hasValidWidth && !hasValidHeight:
-					this.report({
-						node: target,
-						message:
-							"<img> element must have both 'width' and 'height' attributes.",
-					});
-					break;
-				case !width:
-					this.report({
-						node: target,
-						message: "<img> element is missing the 'width' attribute.",
-					});
-					break;
-				case !height:
-					this.report({
-						node: target,
-						message: "<img> element is missing the 'height' attribute.",
-					});
-					break;
-				case !hasModernImageFmt:
-					this.report({
-						node: target,
-						message:
-							"Use modern image formats like WebP or AVIF instead of PNG/JPG/JPEG for better performance.",
-					});
-					break;
-				default:
-					break;
+				const srcValue = img.getAttributeValue("src");
+				const hasModernImageFmt =
+					typeof srcValue === "string" &&
+					/\.(webp|avif)(\?.*)?$/i.test(srcValue);
+				switch (true) {
+					case !hasValidWidth && !hasValidHeight:
+						this.report({
+							node: img,
+							message:
+								"<img> element must have both 'width' and 'height' attributes.",
+						});
+						break;
+					case !width:
+						this.report({
+							node: img,
+							message: "<img> element is missing the 'width' attribute.",
+						});
+						break;
+					case !height:
+						this.report({
+							node: img,
+							message: "<img> element is missing the 'height' attribute.",
+						});
+						break;
+					case !hasModernImageFmt:
+						this.report({
+							node: img,
+							message:
+								"Use modern image formats like WebP or AVIF instead of PNG/JPG/JPEG for better performance.",
+						});
+						break;
+					default:
+						break;
+				}
 			}
 		});
 	}
